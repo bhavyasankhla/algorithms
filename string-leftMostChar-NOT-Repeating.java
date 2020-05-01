7fc40773059dbfeca74ffd612fa5f41b26daa108
Bhavya Sankhla <bhavyasankhla@gmail.com>
2020-05-01 17:07:24 +0530

    Update string-leftMostChar-NOT-Repeating.java

--- string-leftMostChar-NOT-Repeating.java
+++ string-leftMostChar-NOT-Repeating.java
@@ -5,7 +5,7 @@ import java.io.*;
 
 class Codechef
 {
-	public static void main (String[] args) throws java.lang.Exception
+	public static void main2 (String[] args) throws java.lang.Exception
 	{
 		String s = "geeksforgeeks";
 		char[] ch = s.toCharArray();
@@ -32,5 +32,40 @@ class Codechef
             }
         }
 		
+	}
+	
+	// OPTIMISED APPROACH 
+	
+		public static void main (String[] args) throws java.lang.Exception
+	{
+		String s = "geeksforgeeks";
+		char[] ch = s.toCharArray();
+		int n = s.length();
+		
+		int[] presentChars = new int[256];
+		Arrays.fill(presentChars, -1);
+		// array to keep count if the character is present in s. 
+		
+		int result = -1;
+		
+		for( int i = n-1; i>=0; i--) {
+		    
+		    Integer j = (int) ch[i]; // ascii value of the Character 
+		    
+		    if( presentChars[j] == -1 ) {
+		        presentChars[j] = i;
+		        result = i;
+		        
+	          // Character is appearing first time. 
+		        // Updating result while traversing from right to left. 
+		        // this stores the leftmost index of the char which is NOT repeating
+		    } 
+		}
+		
+		if(result == -1) {
+		    System.out.println("No repeating Character");
+		} else {
+		    System.out.println(ch[result]);
+		}
 	}
 }
