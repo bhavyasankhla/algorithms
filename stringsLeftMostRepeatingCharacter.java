0475726967773fe52b925e67b67df1f63cedea1e
Bhavya Sankhla <bhavyasankhla@gmail.com>
2020-05-01 17:05:29 +0530

    Update stringsLeftMostRepeatingCharacter.java

--- stringsLeftMostRepeatingCharacter.java
+++ stringsLeftMostRepeatingCharacter.java
@@ -8,7 +8,7 @@ import java.io.*;
 /* Name of the class has to be "Main" only if the class is public. */
 class Codechef
 {
-	public static void main (String[] args) throws java.lang.Exception
+	public static void main2 (String[] args) throws java.lang.Exception
 	{
 		String s = "geeksforgeeks";
 		
@@ -37,5 +37,40 @@ class Codechef
 		}
 		
 		System.out.println(result);
+	}
+	
+		// optimised approach 
+	
+		public static void main (String[] args) throws java.lang.Exception
+	{
+		String s = "geeksforgeeks";
+		char[] ch = s.toCharArray();
+		int n = s.length();
+		
+		int[] presentChars = new int[256];
+		Arrays.fill(presentChars, -1);
+		// array to keep count if the character is present in s. 
+		
+		int result = -1;
+		
+		for( int i = n-1; i>=0; i--) {
+		    
+		    Integer j = (int) ch[i]; // ascii value of the Character 
+		    
+		    if( presentChars[j] == -1 ) {
+		        presentChars[j] = i;
+		    } else {
+		        // Character is appearing more than one time. 
+		        // Updating result while traversing from right to left. 
+		        // this stores the leftmost index of the char which is repeating
+		        result = i;
+		    }
+		}
+		
+		if(result == -1) {
+		    System.out.println("No repeating Character");
+		} else {
+		    System.out.println(ch[result]);
+		}
 	}
 }
