13d3e3f7e13b48c4722a4b3134d67cdbad69c52f
Bhavya Sankhla <bhavyasankhla@gmail.com>
2020-05-03 22:05:20 +0530

    Update letter-combinations-of-a-phone-number.java

--- letter-combinations-of-a-phone-number.java
+++ letter-combinations-of-a-phone-number.java
@@ -1,3 +1,24 @@
+// 17. Letter Combinations of a Phone Number
+// Medium
+
+// 3485
+
+// 383
+
+// Add to List
+
+// Share
+// Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
+
+// A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+
+
+// Example:
+
+// Input: "23"
+// Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+
 class Solution {
     public List<String> letterCombinations(String digits) {
      
