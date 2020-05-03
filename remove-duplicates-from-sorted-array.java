9ba2d1460db4384b2c428fba30eb0517f3fa2770
Bhavya Sankhla <bhavyasankhla@gmail.com>
2020-05-03 16:04:25 +0530

    Update remove-duplicates-from-sorted-array.java

--- remove-duplicates-from-sorted-array.java
+++ remove-duplicates-from-sorted-array.java
@@ -1,28 +1,28 @@
-Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+// Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
 
-Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+// Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
 
-Given nums = [0,0,1,1,1,2,2,3,3,4],
+// Given nums = [0,0,1,1,1,2,2,3,3,4],
 
-Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively.
+// Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively.
 
-It doesn't matter what values are set beyond the returned length.
-Clarification:
+// It doesn't matter what values are set beyond the returned length.
+// Clarification:
 
-Confused why the returned value is an integer but your answer is an array?
+// Confused why the returned value is an integer but your answer is an array?
 
-Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
+// Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
 
-Internally you can think of this:
+// Internally you can think of this:
 
-// nums is passed in by reference. (i.e., without making a copy)
-int len = removeDuplicates(nums);
+// // nums is passed in by reference. (i.e., without making a copy)
+// int len = removeDuplicates(nums);
 
-// any modification to nums in your function would be known by the caller.
-// using the length returned by your function, it prints the first len elements.
-for (int i = 0; i < len; i++) {
-    print(nums[i]);
-}
+// // any modification to nums in your function would be known by the caller.
+// // using the length returned by your function, it prints the first len elements.
+// for (int i = 0; i < len; i++) {
+//     print(nums[i]);
+// }
 
 
 
