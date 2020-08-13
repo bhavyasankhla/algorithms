747f0859626c8127cbbe6dc82ecf07817e859a1c
Bhavya Sankhla <bhavyasankhla@gmail.com>
2020-08-13 18:33:26 +0530

    Create detect-cycle-ll.java

---
+++ linked-lists/detect-cycle-ll.java
@@ -0,0 +1,34 @@
+/**
+Given a linked list, determine if it has a cycle in it.
+
+To represent a cycle in the given linked list, 
+we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to.
+If pos is -1, then there is no cycle in the linked list.
+
+* Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+            
+            ListNode slow = head, fast = head;
+            
+            while(fast != null && fast.next != null ) {                    
+                    fast = fast.next.next;
+                    slow = slow.next;
+                    if(slow==fast) {
+                            return true;
+                    }
+            }
+            
+            return false;
+        
+    }
+}
