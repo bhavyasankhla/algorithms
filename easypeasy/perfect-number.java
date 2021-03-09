d032a14d6571b44f4fc7baf2e5f1ae1de825e44e
Bhavya Sankhla <bhavyasankhla@gmail.com>
2021-03-09 13:48:19 +0530

    Create perfect-number.java

---
+++ easypeasy/perfect-number.java
@@ -0,0 +1,76 @@
+/**
+
+https://leetcode.com/problems/perfect-number/
+507. Perfect Number
+Easy
+
+361
+
+714
+
+Add to List
+
+Share
+A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself. A divisor of an integer x is an integer that can divide x evenly.
+
+Given an integer n, return true if n is a perfect number, otherwise return false.
+
+ 
+
+Example 1:
+
+Input: num = 28
+Output: true
+Explanation: 28 = 1 + 2 + 4 + 7 + 14
+1, 2, 4, 7, and 14 are all divisors of 28.
+Example 2:
+
+Input: num = 6
+Output: true
+Example 3:
+
+Input: num = 496
+Output: true
+Example 4:
+
+Input: num = 8128
+Output: true
+Example 5:
+
+Input: num = 2
+Output: false
+ 
+
+Constraints:
+
+1 <= num <= 108
+
+**/
+
+class Solution {
+    public boolean checkPerfectNumber(int N) {
+        
+        if(N==1) {
+            return false;
+        }
+
+        int sum = 1;
+      
+        
+      // This is an important concept. 
+      // ex -  28 is divisible by 2, then 14 is also a divisor which is 28/2
+      // So we only need to check till sqrt of N 
+            
+        for(int i = 2; i<=Math.sqrt(N); i++) {
+            if(N%i==0) {
+                    sum = sum + i + N/i;
+            }
+        }
+
+        if(sum == N) {
+            return true;
+        }
+
+        return false;
+    }
+}
